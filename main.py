1ae3d83833068452ceab6aabce1e3689d2c10ea6
Amir Hacohen <30296228+amirhac@users.noreply.github.com>
2021-05-16 16:27:33 +0300

    mqtt

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from datetime import datetime
+import json
+
 import requests
 import yaml
-
+import paho.mqtt.client as mqtt
 
 with open(r'config.yaml') as file:
     config = yaml.load(file, Loader=yaml.FullLoader)
@@ -11,29 +12,49 @@ AREAS = config['AREAS']
 TELEGRAM_BOT_TOKEN = config['TELEGRAM_BOT_TOKEN']
 CHAT_IDS = config['CHAT_IDS']
 
-def is_red_alert():
-    alerts = requests.get("https://www.oref.org.il/WarningMessages/History/AlertsHistory.json").json()
-    true_alerts = []
-    for alert in alerts:
-        alert_time = datetime.strptime(alert['alertDate'], '%Y-%m-%d %H:%M:%S')
-        area = alert['data']
-        seconds_since_alert = (datetime.now() - alert_time).total_seconds()
-        if seconds_since_alert < SECONDS_THRESHOLD and area in AREAS:
-            true_alerts.append(alert)
-            print(alert)
+telegram_enabled = False
 
-    if true_alerts:
-        return true_alerts
+mqttc = mqtt.Client("redalert")
+mqttc.connect("localhost", 1884)
+mqttc.loop_start()
+
+
+def is_red_alert():
+    headers = {
+        "Referer": "https://www.oref.org.il/12481-he/Pakar.aspx".encode(),
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8".encode(),
+        "X-Requested-With": "XMLHttpRequest",
+    }
+
+    alerts = requests.get("http://www.oref.org.il/WarningMessages/Alert/alerts.json", headers=headers).text
+    if alerts != "":
+        data = json.loads(alerts)
+        areas = set(data['data']) & set(AREAS)
+        if areas or True:
+            msg = "RED ALERT in " + str(data)
+            print(msg)
+            send_telegram(msg)
+            send_mqtt("on")
+        else:
+            send_mqtt("off")
     else:
-        return None
+        send_mqtt("off")
 
 
 def send_telegram(msg):
-    url = "https://api.telegram.org/bot%s/sendMessage" % TELEGRAM_BOT_TOKEN
-    for id in CHAT_IDS:
-        requests.post(url, data={"chat_id": id, "text": msg})
+    if telegram_enabled:
+        url = "https://api.telegram.org/bot%s/sendMessage" % TELEGRAM_BOT_TOKEN
+        for id in CHAT_IDS:
+            requests.post(url, data={"chat_id": id, "text": msg})
 
 
-alerts = is_red_alert()
-if alerts:
-    send_telegram("RED ALERT\n" + alerts[0]['alertDate'] + "\n" + alerts[0]['data'])
+def send_mqtt(msg):
+    mqttc.publish("redalert/alert", msg)
+    mqttc.loop_stop()
+
+
+try:
+    while True:
+        is_red_alert()
+finally:
+    mqttc.loop_stop()
